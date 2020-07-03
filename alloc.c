6cb3d42ac86b1a2572f0ed57c939de4068c8393e
Patrick Gingras <775.pg.12@gmail.com>
2020-07-02 23:14:07 -0400

    some safety and cleanup

--- alloc.c
+++ alloc.c
@@ -1,3 +1,4 @@
+#include <assert.h>
 #include <stdlib.h>
 
 #include "alloc.h"
@@ -7,6 +8,7 @@ void *cb_malloc(size_t bytes, cb_deinit_fn deinit_fn)
 {
 	void *ptr;
 
+	assert(bytes >= sizeof(cb_gc_header));
 	ptr = malloc(bytes);
 	cb_gc_register((cb_gc_header*) ptr, deinit_fn);
 
