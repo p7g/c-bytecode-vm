3f953108f95a38239a9aa152e7bf42d58042cdb7
Patrick Gingras <775.pg.12@gmail.com>
2021-04-28 00:23:34 -0400

    added string.from_bytes function

--- modules/strings.c
+++ modules/strings.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include "builtin_modules.h"
 #include "bytes.h"
@@ -10,7 +11,8 @@
 #include "value.h"
 
 size_t ident_len,
-       ident_char_at;
+       ident_char_at,
+       ident_from_bytes;
 
 static int len(size_t argc, struct cb_value *argv, struct cb_value *result)
 {
@@ -42,10 +44,29 @@ static int char_at(size_t argc, struct cb_value *argv, struct cb_value *result)
 	return 0;
 }
 
+static int from_bytes(size_t argc, struct cb_value *argv,
+		struct cb_value *result)
+{
+	cb_str str;
+	struct cb_bytes *bs;
+
+	CB_EXPECT_TYPE(CB_VALUE_BYTES, argv[0]);
+	bs = argv[0].val.as_bytes;
+
+	cb_str_init(&str, cb_bytes_len(bs));
+	memcpy(cb_strptr(&str), cb_bytes_ptr(bs), cb_bytes_len(bs));
+
+	result->type = CB_VALUE_STRING;
+	result->val.as_string = cb_string_new();
+	result->val.as_string->string = str;
+	return 0;
+}
+
 void cb_strings_build_spec(cb_modspec *spec)
 {
 	CB_DEFINE_EXPORT(spec, "len", ident_len);
 	CB_DEFINE_EXPORT(spec, "char_at", ident_char_at);
+	CB_DEFINE_EXPORT(spec, "from_bytes", ident_from_bytes);
 }
 
 void cb_strings_instantiate(struct cb_module *mod)
@@ -53,4 +74,6 @@ void cb_strings_instantiate(struct cb_module *mod)
 	CB_SET_EXPORT(mod, ident_len, cb_cfunc_new(ident_len, 1, len));
 	CB_SET_EXPORT(mod, ident_char_at,
 			cb_cfunc_new(ident_char_at, 1, char_at));
+	CB_SET_EXPORT(mod, ident_from_bytes,
+			cb_cfunc_new(ident_from_bytes, 1, from_bytes));
 }
