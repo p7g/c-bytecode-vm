bd79491247bdf281e9eb9f987d1972b8cb29872f
github-action-benchmark <github@users.noreply.github.com>
2022-03-31 04:45:53 +0000

    add c-bytecode-vm benchmark (customSmallerIsBetter) benchmark result for ba3a822c3aed8cbfeadde33f892cb9ea8b9a253b

---
+++ dev/bench/data.js
@@ -0,0 +1,37 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1648701953114,
+  "repoUrl": "https://github.com/p7g/c-bytecode-vm",
+  "entries": {
+    "c-bytecode-vm benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "775.pg.12@gmail.com",
+            "name": "Patrick Gingras",
+            "username": "p7g"
+          },
+          "committer": {
+            "email": "775.pg.12@gmail.com",
+            "name": "Patrick Gingras",
+            "username": "p7g"
+          },
+          "distinct": true,
+          "id": "ba3a822c3aed8cbfeadde33f892cb9ea8b9a253b",
+          "message": "add benchmark action",
+          "timestamp": "2022-03-31T00:30:24-04:00",
+          "tree_id": "5f4f53abbd33ec3b6c5b2e9a03e70a44bd6d26de",
+          "url": "https://github.com/p7g/c-bytecode-vm/commit/ba3a822c3aed8cbfeadde33f892cb9ea8b9a253b"
+        },
+        "date": 1648701952509,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "bench.b",
+            "value": 234306,
+            "unit": "ms"
+          }
+        ]
+      }
+    ]
+  }
+}
