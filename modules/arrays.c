8ee8c011b9ca1e528a6ca9fe53cfb914d53ed577
Patrick Gingras <775.pg.12@gmail.com>
2025-01-11 13:15:40 -0500

    allow specifying init value in array.new

--- modules/arrays.c
+++ modules/arrays.c
@@ -14,24 +14,27 @@ void cb_arrays_build_spec(cb_modspec *spec)
 static int array_new(size_t argc, struct cb_value *argv,
 		struct cb_value *result)
 {
-	struct cb_value len_val;
+	struct cb_value len_val, init_val;
 	int64_t len;
 	len_val = argv[0];
 
 	CB_EXPECT_TYPE(CB_VALUE_INT, len_val);
 	len = len_val.val.as_int;
-
 	if (len < 0) {
 		cb_error_set(cb_value_from_string("array_new: Invalid length"));
 		return 1;
 	}
+
+	init_val.type = CB_VALUE_NULL;
+	if (argc > 1)
+		init_val = argv[1];
 
 	result->type = CB_VALUE_ARRAY;
 	result->val.as_array = cb_array_new((size_t) len);
 	result->val.as_array->len = len;
 
 	for (int i = 0; i < len; i += 1)
-		result->val.as_array->values[i].type = CB_VALUE_NULL;
+		result->val.as_array->values[i] = init_val;
 
 	return 0;
 }
